002c1174c2409338def36ca14c3da3374deadf59
Alan Herculano Diniz <alanhdiniz@gmail.com>
2023-06-27 22:36:12 -0300

    Changing the function parameters

--- explicit_ivp.m
+++ explicit_ivp.m
@@ -1,4 +1,4 @@
-function [x, y, t, u] = explicit_ivp(bounds, T, m, n, dt, initial_condition, bound_conditions)
+function [x, y, t, u] = explicit_ivp(bounds, T, m, n, dt, initial_condition, bound_conditions, kappa, beta_func, gamma_func, g_func, h_func, f_func)
 
     x = linspace(bounds.a, bounds.b, n);
     y = linspace(bounds.c, bounds.d, m);
